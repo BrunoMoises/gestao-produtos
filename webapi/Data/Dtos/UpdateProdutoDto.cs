5b59d5b25116b1926ae5e3468ace4662af81501b
Bruno <bruno_moises_stz@hotmail.com>
2023-04-05 19:20:12 -0300

    feat: criando dto de atualização do produto do produto

---
+++ webapi/Data/Dtos/UpdateProdutoDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace webapi.Data.Dtos;
+
+public class UpdateProdutoDto
+{
+    [Required(ErrorMessage = "O título do produto é obrigatório!")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "A descrição do produto é obrigatório!")]
+    public string Descricao { get; set; }
+    public float Valor { get; set; }
+}
