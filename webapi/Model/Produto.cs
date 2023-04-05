f36986ba3a329224b20bad0576511b2e929f88ee
Bruno <bruno_moises_stz@hotmail.com>
2023-04-05 15:11:54 -0300

    feat: criando model inicial de produto

---
+++ webapi/Model/Produto.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.Model;
+
+[Table("produto")]
+public class Produto
+{
+    [Key]
+    [Required]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O título do produto é obrigatório!")]
+    [Column("titulo")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "A descrição do produto é obrigatório!")]
+    [Column("descricao")]
+    public string Descricao { get; set; }
+
+    [Column("imagem")]
+    public byte[] Imagem { get; set; }
+}
